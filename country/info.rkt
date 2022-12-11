86b1b7f3dcec4a2edf6ac25189fe01bf532f9eb5
Bogdan Popa <bogdan@defn.io>
2022-12-11 19:36:42 +0200

    build: fixup license sexp

--- country/info.rkt
+++ country/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
-(define license '(BSD-3-Clause AND ODbL-1.0 AND GPL-2.0-or-later))
+(define license '((BSD-3-Clause AND ODbL-1.0) AND GPL-2.0-or-later))
 (define version "2022-12-11")
 (define collection "country")
 
