e433bcdb710718f3b46470fc2cf6f210c98998e1
Bogdan Popa <bogdan@defn.io>
2023-02-27 13:06:00 +0200

    build: make version compatible with valid-version?

--- country/info.rkt
+++ country/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define license '((BSD-3-Clause AND ODbL-1.0) AND GPL-2.0-or-later))
-(define version "2022-12-11")
+(define version "20221211.0")
 (define collection "country")
 
 (define deps '("base"))
